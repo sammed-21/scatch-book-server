13971ad6d4ce9d1157d35b3b7d4bbeecc6007baa
sammed-21 <sammed2110@gmail.com>
2023-10-08 08:44:05 +0530

    fix the production error

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,10 @@ const express_1 = __importDefault(require("express"));
 const http_1 = require("http");
 const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
+require('dotenv').config(); // Load environment variables from .env file
 const app = (0, express_1.default)();
 const isDev = app.settings.env === 'development';
-const URL = isDev ? 'http://localhost:3000' : 'http://localhost:3000';
+const URL = isDev ? 'http://localhost:3000' : 'https://scatch-book.vercel.app/';
 app.use((0, cors_1.default)({ origin: URL }));
 const httpServer = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(httpServer, { cors: { origin: URL } });
@@ -18,6 +19,7 @@ io.on('connection', (socket) => {
     socket.on('beginPath', (arg) => {
         socket.broadcast.emit('beginPath', arg);
     });
+    console.log(URL);
     socket.on('drawLine', (arg) => {
         socket.broadcast.emit('drawLine', arg);
     });
@@ -27,5 +29,6 @@ io.on('connection', (socket) => {
 });
 const PORT = 5000;
 httpServer.listen(PORT, () => {
+    console.log(app.settings.env);
     console.log(`Server is running on port ${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server as SocketServer, Socket } from 'socket.io';
 import cors from 'cors';
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const isDev = app.settings.env === 'development';
-const URL = isDev ? 'http://localhost:3000' : 'https://scatch-book.vercel.app/';
+ 
+const URL = isDev ? 'http://localhost:3000' : 'https://scatch-book.vercel.app';
+ 
 app.use(cors({origin: URL}));
 interface chageConfigs  {
     color?: string;
     size?:number
 }
+ 
 const httpServer = createServer(app);
 const io = new SocketServer(httpServer, { cors: { origin: URL } });
 
@@ -20,6 +24,7 @@ io.on('connection', (socket: Socket) => {
   socket.on('beginPath', (arg: MouseEvent) => {
     socket.broadcast.emit('beginPath', arg);
   });
+ 
 
   socket.on('drawLine', (arg: MouseEvent) => {
     socket.broadcast.emit('drawLine', arg);
@@ -34,5 +39,6 @@ io.on('connection', (socket: Socket) => {
 const PORT: number = 5000;
 
 httpServer.listen(PORT, () => {
+  console.log(app.settings.env)
   console.log(`Server is running on port ${PORT}`);
 });
